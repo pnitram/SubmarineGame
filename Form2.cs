c9e3244acec191c261132d46790e37fdb126a5c7
pnitram <martin.pedersen@me.com>
2017-02-26 21:02:40 +0100

    Fungerende Serialisering

--- Form2.cs
+++ Form2.cs
@@ -1,11 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Windows.Forms;
+using System.Xml.Serialization;
 
 namespace SubmarineGame
 {
     public partial class GameOverForm : Form
     {
-        public GameOverForm()
+
+        private List<AddHighScore> _highScores;
+        private AddHighScore _score;
+
+        public GameOverForm()   
         {
             InitializeComponent();
             label3.Text = "Du oppnådde: " + Form1.Poeng + " poeng!";
@@ -20,6 +28,44 @@ namespace SubmarineGame
         private void label3_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                textBox1.Text = "Annonym spiller";
+            }
+
+            if (File.Exists("scores.dat") )
+            {
+                using (var fileStream = new FileStream("scores.dat", FileMode.Open, FileAccess.ReadWrite))
+                {
+                    var formatter = new BinaryFormatter();
+                    _highScores = (List<AddHighScore>)formatter.Deserialize(fileStream);
+                }
+                _score = new AddHighScore() { Score = Form1.Poeng, PlayerName = textBox1.Text };
+                _highScores.Add(_score);
+
+                using (var fileStream = new FileStream("scores.dat", FileMode.Create, FileAccess.Write))
+                {
+                    var formatter = new BinaryFormatter();
+                    formatter.Serialize(fileStream, _highScores);
+                }
+
+
+            }
+            
+
+/*            using (var fileStream = new FileStream("scores.dat", FileMode.Append, FileAccess.Write))
+                {
+                    var formatter = new BinaryFormatter();
+                    formatter.Serialize(fileStream, _highScores);
+                }
+
+            }*/
+
+            
         }
     }
 }
